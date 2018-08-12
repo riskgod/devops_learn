d4624f3de5d333d4775d089540ffcccdcb6fab19
bruce <riskgod.chen@pwc.com>
2018-08-12 22:01:27 +0800

    first

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM 		tomcat:8.0.21-jre8
+
+COPY 		./software/ /usr/local/tomcat/webapps/
+
---
+++ setup.sh
@@ -0,0 +1,5 @@
+unzip -n ./software/test.zip -d /software
+
+
+docker-machine create --driver virtualbox myvm1
+docker-machine create --driver virtualbox myvm2
